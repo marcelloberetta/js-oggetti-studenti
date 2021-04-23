a618009a601afb1c620d0eda37ed4a20c01edc8e
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-23 18:09:54 +0200

    Esercizio finito

--- script.js
+++ script.js
@@ -14,9 +14,68 @@ for (var key in studenti){
 }
 
 
+console.log("-----------------------------");
+
+//CREO UN ARRAY DI OGGETTI
+
+var students = [
+   {
+   nome: "Marcello",
+   cognome: "Beretta",
+   età: 30,
+   },
+
+  {
+     nome: "Giovanni",
+     cognome: "Storti",
+     età: 64,
+  },
+
+  {
+     nome: "Cataldo",
+     cognome: "Baglio",
+     età: 62,
+  },
 
 
+  {
+     nome: "Giacomino",
+     cognome: "Poretti",
+     età: 64,
+  },
+
+  {
+     nome: "Eros",
+     cognome: "Cecconi",
+     età: 75,
+  }
+
+];
+
+for(var index in students){
+   var a = students[index]
+   console.log( a['nome'],a['cognome'] );
+
+}
+
+alert("Ora ti permetterò di inserire i dati di un nuovo studente");
+
+var nuovoNome = prompt("Inserisci il nome dello studente");
+
+var nuovoCognome = prompt("Inserisci il cognome dello studente");
+
+var nuovaEta = parseInt(prompt("Inserisci l'età dello studente"));
+ 
+
+students.push({
+   "nome" : nuovoNome,
+   "cognome" : nuovoCognome,
+   "età" : nuovaEta,
+} );
 
 
+/* for(var index in students){
+   var a = students[index]
+   console.log( a['nome'],a['cognome'] );
 
-
+} */
