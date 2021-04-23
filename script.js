85255c28be89730772bd3b3b0602566e36269b5f
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-23 15:51:23 +0200

    Object

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//CREO UN OGGETTO  CONTENENTE UNO STUDENTE
+var studenti = {
+   nome: "Franco",
+   cognome: "De Ughi",
+   età: 38,
+};
+
+
+//STAMPO A SCHERMO IL CONTENUTO DELL'OGGETTO
+
+for (var key in studenti){
+   console.log(studenti[key]);
+}
+
+
+
+
+
+
+
+
